0787d53d2662de6db45449da6ad12940f32b7eb4
Wesley <wesy32@gmail.com>
2024-01-18 23:47:57 -0800

    Merge sort psuedocode + test output

--- mergesort.rb
+++ mergesort.rb
@@ -1,12 +1,18 @@
 def merge_sort(arr)
     if (arr.length() == 1)
         return arr
+    else
+        # sort left half
+        
+        # sort right half
+        # merge
     end
-
-
-
+    arr
 end
 
 
-arr = [1]
-puts merge_sort(arr)
+arr = [3, 2, 1, 13, 8, 5, 0, 1]
+
+puts "Ex.array: [0, 1, 1, 2, 3, 5, 8, 13]"
+print "My array: "
+p merge_sort(arr)
